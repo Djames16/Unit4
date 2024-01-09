10a93d3657f538ddd2669e3d2d018825168a1d8f
Djames16 <djames@brooklynsteamcenter.org>
2024-01-09 10:47:04 -0500

    2024-01-09

--- todo.py
+++ todo.py
@@ -20,14 +20,22 @@ todo=["Eat cake","Workout"]
 def index():
     if request.method=='POST':
         new_todo=request.form["new_todo"]
-        todo.append(new_todo)
+        cursor=connect.cursor()
+        cursor.execute(f"INSERT INTO `todos`(`description`) VALUES('{new_todo}')")
+        cursor.close()
+        connect.commit()
     cursor=connect.cursor()
-    cursor.execute("SELECT `description` FROM `todos`")
+    cursor.execute("SELECT * FROM `todos`")
     results=cursor.fetchall()
+    cursor.close()
+
     return render_template("todo.html.jinja", my_todos=results)
 
 @app.route('/delete_todo/<int:todo_index>',methods=['POST'])
 def todo_delete(todo_index):
-    del todo[todo_index]
+    cursor=connect.cursor()
+    cursor.execute(f"DELETE FROM `todos` WHERE `id`={todo_index}")
+    cursor.close()
+    connect.commit()
     return redirect('/')
 
