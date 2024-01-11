284077e1a8dc36e5523f792af8490eb370582837
Djames16 <djames@brooklynsteamcenter.org>
2024-01-11 10:51:43 -0500

    2024-11-01

--- todo.py
+++ todo.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
 from pprint import pprint as print
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app=Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 connect=pymysql.connect(
     database="djames_todos",
@@ -17,6 +31,7 @@ todo=["Eat cake","Workout"]
 
 
 @app.route('/',methods=['GET','POST'])
+@auth.login_required
 def index():
     if request.method=='POST':
         new_todo=request.form["new_todo"]
@@ -29,7 +44,10 @@ def index():
     results=cursor.fetchall()
     cursor.close()
 
+
     return render_template("todo.html.jinja", my_todos=results)
+
+    return "Hello, {}!".format(auth.current_user())
 
 @app.route('/delete_todo/<int:todo_index>',methods=['POST'])
 def todo_delete(todo_index):
